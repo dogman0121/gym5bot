7b0150c3e0747502bdc88b099c69b57435e81c10
Александра Васильева <ivanvasilev@gym5cheb.ru>
2022-03-30 20:14:23 +0300

    release v1.0

--- browser.py
+++ browser.py
@@ -1,36 +1,101 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
+import re
 import time
+
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 class WebFunction:
 
+    # подключение к вебдрайверу
     def __init__(self):
-        s = Service("D:\\pythonProjects\\gym5bot\\chromedriver.exe")
-        self.browser = webdriver.Chrome(service=s)
-        self.browser.set_window_size(1920,1080)
+        self.s = Service("D:\\pythonProjects\\gym5bot\\chromedriver.exe")
+        self.browser = webdriver.Chrome(service=self.s)
+        self.browser.set_window_size(1920, 1080)
+        self.headers = {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
+            'accept-language': 'en-US,en;q=0.9,ru;q=0.8'
+        }
+        self.way = {
+            "А": "https://gim5cheb.edupage.org/substitution/server/viewer.js",
+            "Т": "https://gym5cheb.edupage.org/substitution/server/viewer.js"
+        }
 
-    def get_schedule(self, name, surname, grade, symbol, built):
+    # получение скриншота расписания
+    def get_schedule(self, data):
+        name, surname, grade, symbol, built = data
+        flag = False
+
+        wait = WebDriverWait(self.browser, 10)
         self.browser.get("https://gim5cheb.edupage.org/timetable/")
-        time.sleep(3)
+        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[title='Учащиеся']")))
         self.browser.find_element(By.CSS_SELECTOR, "[title='Учащиеся']").click()
-        time.sleep(1)
-        if int(grade) > 9:
-            for number in self.browser.find_elements(By.CSS_SELECTOR, "[class = 'submenu']"):
-                if number.text == f"{grade}/{built}":
-                    number.click()
-        else:
-            for number in self.browser.find_elements(By.CSS_SELECTOR, "[class = 'submenu']"):
-                if number.text == f"{grade}{symbol}/{built}":
-                    number.click()
+        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[class = 'submenu']")))
 
-        time.sleep(1)
-        for student in self.browser.find_elements(By.CSS_SELECTOR, "[class = 'dropDownPanel asc-context-menu'] li"):
+        if int(grade) > 9:
+            for item in self.browser.find_elements(By.CSS_SELECTOR, "[class = 'submenu']"):
+                if item.text == f"{grade}/{built}":
+                    flag = True
+                    item.click()
+                    break
+        else:
+            for item in self.browser.find_elements(By.CSS_SELECTOR, "[class = 'submenu']"):
+                if item.text == f"{grade}{symbol}/{built}":
+                    flag = True
+                    item.click()
+                    break
+        if not flag:
+            return 0
+
+        flag = False
+
+        wait.until(EC.element_to_be_clickable((item)))
+
+        for student in item.find_elements(By.CSS_SELECTOR, "[class = 'dropDownPanel asc-context-menu'] li"):
             if f"{surname} {name}" in student.text:
+                flag = True
                 student.click()
-        time.sleep(1)
-        schedule = self.browser.find_element(By.ID, "skin_PageContent_2")
-        path = f"{name}_{surname}_{grade}{symbol}_{built}"
-        with open(f"temp_picture\\{path}.png", "wb") as file:
-            file.write(schedule.screenshot_as_png)
-        return path
+                break
+
+        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[class = 'print-nobreak']")))
+        schedule = self.browser.find_element(By.CSS_SELECTOR, "[class = 'print-nobreak']")
+
+        time.sleep(0.5)
+
+        if flag:
+            return schedule.screenshot_as_png
+        else:
+            return 0
+
+    # получение расписания в виде json
+    def get_changes(self, date, built):
+        changes = {}
+        params = (
+            ('__func', 'getSubstViewerDayDataHtml'),
+        )
+
+        data = '{"__args":[null,{"date":"' + date + '","mode":"classes"}],"__gsh":"00000000"}'
+
+        r = requests.post(self.way[built], headers=self.headers, params=params, data=data)
+        soup = BeautifulSoup(r.text, "lxml")
+        changesList = [item.text for item in soup.find_all("span")]
+        if "Для этого дня замен нет." in changesList:
+            return "Для этого дня замен нет."
+        for item in changesList:
+            if not (re.search(r"^\d+(\w)( )?/(\w)$", item, flags=re.MULTILINE) is None):
+                grade = item
+                changes[grade] = {}
+            elif (not (re.search(r"^\d?(.) - \d?(.)$", item, flags=re.MULTILINE) is None)
+                  or not (re.search(r"^\d?(.)$", item, flags=re.MULTILINE) is None)
+                  or not (re.search(r"^\(\d?(.) - \d?(.)\)$", item, flags=re.MULTILINE) is None)
+                  or not (re.search(r"^\(\d?(.)\)$", item, flags=re.MULTILINE) is None)):
+                lessonNumber = item
+            else:
+                change = item
+                changes[grade][lessonNumber] = change
+        return changes
--- main.py
+++ main.py
@@ -1,27 +1,143 @@
-from aiogram import Bot, Dispatcher, executor, types
-
 import asyncio
+import json
 import os
 import re
-from TOKEN import TOKEN
+from datetime import datetime, timedelta
 
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.utils import exceptions
+
+from TOKEN import TOKEN
 from browser import WebFunction
+from database import Database
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 browser = WebFunction()
+database = Database()
+
+loop = asyncio.get_event_loop()
+
+with open("books_data.json", "r", encoding="utf-8") as f:
+    books = json.load(f)
 
 
+# функция по парсингу и обработке замен
+async def timeprocess():
+    while True:
+        for built in ["А", "Т"]:
+            dateTomorrow = (datetime.now() + timedelta(days=1)).strftime('%Y-%m-%d')
+            changesResult = browser.get_changes(dateTomorrow, built)
+            if not os.path.exists(f"zameny\\{dateTomorrow}_{built}.json"):
+                if not (changesResult == "Для этого дня замен нет."):
+                    with open(f"zameny\\{dateTomorrow}_{built}.json", "w", encoding="utf-8") as file:
+                        json.dump(changesResult, file, indent=2, ensure_ascii=False)
+                    for classNumber in changesResult.keys():
+                        changesString = classNumber + "\n"
+                        for lessonNumber in changesResult[classNumber].keys():
+                            changesString += lessonNumber + ": " + changesResult[classNumber][lessonNumber] + "\n"
+                        cs = re.findall(r"(\d{1,2})(\w{0,1}?)( )?/(\w)", classNumber)
+                        if cs[0][1] == "":
+                            spisokStudents = database.user_data_by_params(grade=cs[0][0], built=cs[0][3])
+                        else:
+                            spisokStudents = database.user_data_by_params(grade=cs[0][0], symbol=cs[0][1],
+                                                                          built=cs[0][3])
+                        for student in spisokStudents:
+                            try:
+                                await bot.send_message(student[0],
+                                                       f"Расписание на следующий день доступно: \n{changesString}")
+                            except exceptions.ChatNotFound:
+                                pass
+        await asyncio.sleep(3600)
+
+
+# кнопка старт
+@dp.message_handler(commands=["start"])
+async def start(message: types.Message):
+    await bot.send_message(message.from_user.id,
+                           "Привет! Чтобы было проще пользоваться функциями бота авторизируйся. "
+                           "Напиши информацию о себе. <Имя> <Фамилия> <Класс> <Корпус>."
+                           "\nНапример: Иван Иванов 7А А")
+
+
+# отправление расписания по запросу
 @dp.message_handler(commands=["schedule"])
 async def message(message: types.Message):
-    inf = re.findall(r"(/schedule) (\w+) (\w+) (\d+) (\w) (\w)", message.text)[0]
-    result = browser.get_schedule(inf[1], inf[2], inf[3], inf[4], inf[5])
-    with open(f"temp_picture//{result}.png", "rb") as file:
-        await bot.send_photo(message.from_user.id, file, caption="Вот ваше расписание")
-    os.remove(f"temp_picture//{result}.png")
+    user_inf = re.findall(r"(/schedule) (\w+) (\w+) (\d+) (\w) (\w)", message.text)
+    if len(user_inf) > 0:
+        timetable = browser.get_schedule(user_inf[0][1:])
+    else:
+        if database.check_user(message.from_user.id):
+            timetable = browser.get_schedule(database.get_user_data(message.from_user.id)[0][1:])
+        else:
+            timetable = 0
+    if timetable != 0:
+        await bot.send_photo(message.from_user.id, timetable, caption="Вот ваше расписание")
+    else:
+        await bot.send_message(message.from_user.id,
+                               "Похоже вы неправильно указали ваши данные для просмотра расписания")
 
-    #await bot.send_message(message.from_user.id, message.text)
+
+# изменение данный пользователя в дб
+@dp.message_handler(commands=["changename"])
+async def change_name(message: types.Message):
+    if not (re.search(r"\w+ \w+ \d+\w \w", message.text) is None):
+        info_student = re.findall(r"(\w+) (\w+) (\d+)(\w) (\w)", message.text)[0]
+        database.update_user(message.from_user.id, info_student)
+        await bot.send_message(message.from_user.id, "Ваши данные успешно изменены.")
+    else:
+        await bot.send_message(message.from_user.id,
+                               "Проверьте правильность написания ваших данных. Образец: Иванов Иван 7А А")
+
+
+# поиск книги в базе данных
+@dp.message_handler(commands="library")
+async def searchbook(message: types.Message):
+    if message.text == "/library":
+        await bot.send_message(message.from_user.id,
+                               "Мы не можем определить ваш запрос. Пожалуйста, напишите подробнее.")
+    else:
+        s = ""
+        for id in books.keys():
+            print(id)
+            if any(not (re.search(i, books[id]["name"], flags=re.IGNORECASE) is None) for i in
+                   message.text[8:].split()):
+                s += f"*{books[id]['name']}*\nСкачать: /download{id}\n\n"
+        if s == "":
+            await bot.send_message(message.from_user.id, f"По запросу \"{message.text[8:]}\" ничего не нашлось.")
+        else:
+            await bot.send_message(message.from_user.id, "Вот что мне удалось найти:\n\n" + s, parse_mode="Markdown")
+
+
+# добавляем документ в базу данных
+@dp.message_handler(content_types=["document"])
+async def get_book(message: types.Message):
+    """Если надо будет больше аргументов, используем regex"""
+    file_id = message.document.file_id
+    caption = message.caption
+    books[str(len(books) + 1)] = {"name": caption, "file_id": file_id}
+    with open("books_data.json", "w", encoding="utf-8") as file:
+        json.dump(books, file, indent=2, ensure_ascii=False)
+    await bot.send_message(message.from_user.id, "Книга добавлена")
+
+
+@dp.message_handler(content_types=["text"])
+async def only_text(message: types.Message):
+    if not (re.search(r"\w+ \w+ \d+\w \w", message.text) is None):
+        info_student = re.findall(r"(\w+) (\w+) (\d+)(\w) (\w)", message.text)[0]
+        result = database.add_student(message.from_user.id, info_student)
+        if not result:
+            await bot.send_message(message.from_user.id, "Похоже вы уже авторизированы")
+        else:
+            await bot.send_message(message.from_user.id, "Авторизация прошла успешно")
+    if message.text.startswith("/download"):
+        try:
+            print(books[message.text[9:]]["file_id"])
+            await bot.send_document(message.from_user.id, books[message.text[9:]]["file_id"])
+        except KeyError:
+            await bot.send_message(message.from_user.id, "Данный учебник не был найден")
 
 
 if __name__ == '__main__':
+    loop.create_task(timeprocess())
     executor.start_polling(dp, skip_updates=True)
